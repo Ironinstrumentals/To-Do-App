c36e24f26c3eb352dbb0aa5891dec943a81a320e
David Schlenker <ironinstrumentals@gmail.com>
2019-02-20 14:06:49 -0700

    #08 - It's Alive!
    So far, it seems to be working, after hours and hours of Guessing the Syntax and using Trial And Error, i have finally figured out how to display the Strings inside an array inside an object.

--- JS/Main.js
+++ JS/Main.js
@@ -34,7 +34,7 @@ function CreateList(){
 
 
 
-
+/*
 let addArrayToObject = (object, propertyName, ...strings) => {
     object[propertyName] = strings;
 };
@@ -49,8 +49,43 @@ function CreateList(){
     document.getElementById('SelectBar').innerHTML += '<option>' + listName + '</option>';
 }
 function CreateTaskBox(){
-    document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><label><input  class='Task' value='" + document.getElementById("NewTask").value + "' /></label></div>";
+    document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><label><input  class='Task' value='" + document.getElementById("NewTask").value + "' /></label><button style='margin-left: 3px; padding-top: 1.5px; background-color: red; color: white; border-radius: 5px;'><i class=\"fas fa-times\"></i></button></div>";
 }
 function logObject(){
     console.log(lists)
+}
+*/
+function logObject(){
+    console.log(masterList)
+}
+
+
+masterList = {
+    unassigned: []
+
+};
+
+let addList = function (name) {
+    masterList[name] = [];
+    document.getElementById('SelectBar').innerHTML += '<option id="' + name + '">' + name + '</option>'
+    //Need to make an html element for the list.
+    // '<option> + name + '></option>' for example
+//    so you can target the id to grab the tsk to delete later
+};
+
+
+addTaskToList = function (task) {
+    let name = document.getElementById('SelectBar').value || 'unassigned';
+    masterList[name].push(task);
+    //Need to make an html element for the task.
+    // '<li id="' + task + '"></li>' for example
+//    so you can target the id to grab the tsk to delete later
+};
+function loadList(){
+    let name = document.getElementById('SelectBar').value;
+    document.getElementById('CurrentList').innerHTML = "";
+    for (let i = 0; i < masterList[name].length; i++) {
+            document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><button style='margin-right: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input value='" + masterList[name][i] + "' /></label><button onclick='' style='margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: white; color: green; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
+    }
+    console.log(masterList);
 }
