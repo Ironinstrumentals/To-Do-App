f6d75b9252e4e9f5c950d15a4d61c50472f771fd
David Schlenker <ironinstrumentals@gmail.com>
2019-02-20 14:15:44 -0700

    #08 - Cleaning Script
    Cleaning up the script, removing old comments, and unused code.

--- JS/Main.js
+++ JS/Main.js
@@ -1,60 +1,3 @@
-/*
-let lists = [];
-let tasks = [];
-
-function CreateList(){
-    lists.push(document.getElementById('ListName').value);
-    let result = '';
-    for (let i = 0; i < lists.length; i++) {
-        result+=`<option>${lists[i]}</option>`
-    }
-    document.getElementById('SelectBar').innerHTML = result;
-}
-function CreateTask(){
-    tasks.push(document.getElementById('NewTask').value);
-    let result = '';
-    for (let i = 0; i < tasks.length; i++) {
-        result+=`<div class="FakeDiv"><label><input class="" type="text" value="${tasks[i]}"></label></div>`
-    }
-    document.getElementById('CurrentList').innerHTML = result;
-}
-*/
-/*
-function CreateList(){
-    let propertyName = document.getElementById('ListName').value;
-    let addArrayToObject = (object, ...strings) => {
-        object[propertyName] = strings;
-    };
-    let lists = {};
-    addArrayToObject(lists, 'arrayOne', 'happy', 'flower', 'yummy', 'candy');
-    console.log(lists);
-}
-*/
-
-
-
-
-/*
-let addArrayToObject = (object, propertyName, ...strings) => {
-    object[propertyName] = strings;
-};
-lists = {};
-function CreateList(){
-    let listName = document.getElementById('ListName').value;
-    let task = document.getElementsByClassName('Task');
-    for (let i = 0; i < task.length; i++) {
-        let realTask = task[i].value;
-        addArrayToObject(lists, listName, realTask);
-    }
-    document.getElementById('SelectBar').innerHTML += '<option>' + listName + '</option>';
-}
-function CreateTaskBox(){
-    document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><label><input  class='Task' value='" + document.getElementById("NewTask").value + "' /></label><button style='margin-left: 3px; padding-top: 1.5px; background-color: red; color: white; border-radius: 5px;'><i class=\"fas fa-times\"></i></button></div>";
-}
-function logObject(){
-    console.log(lists)
-}
-*/
 function logObject(){
     console.log(masterList)
 }
@@ -68,18 +11,12 @@ masterList = {
 let addList = function (name) {
     masterList[name] = [];
     document.getElementById('SelectBar').innerHTML += '<option id="' + name + '">' + name + '</option>'
-    //Need to make an html element for the list.
-    // '<option> + name + '></option>' for example
-//    so you can target the id to grab the tsk to delete later
 };
 
 
 addTaskToList = function (task) {
     let name = document.getElementById('SelectBar').value || 'unassigned';
     masterList[name].push(task);
-    //Need to make an html element for the task.
-    // '<li id="' + task + '"></li>' for example
-//    so you can target the id to grab the tsk to delete later
 };
 function loadList(){
     let name = document.getElementById('SelectBar').value;
