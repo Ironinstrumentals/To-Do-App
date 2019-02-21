9031c80f5085df46ffec7afbfe244bcf83864b50
David Schlenker <ironinstrumentals@gmail.com>
2019-02-21 10:18:20 -0700

    #13 - Clear List Button
    Added a button to clear an entire list.

--- JS/Main.js
+++ JS/Main.js
@@ -12,12 +12,32 @@ let addList = function (name) {
 addTaskToList = function (task) {
     let name = document.getElementById('SelectBar').value || 'unassigned';
     masterList[name].push(task);
+    loadList();
 };
 function loadList(){
     let name = document.getElementById('SelectBar').value;
     document.getElementById('CurrentList').innerHTML = "";
     for (let i = 0; i < masterList[name].length; i++) {
-        document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><button style='margin-right: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input value='" + masterList[name][i] + "' /></label><button onclick='' style='margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: white; color: green; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
+        document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><button onclick='deleteTask()' id='btn"+ masterList[name][i] +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input value='" + masterList[name][i] + "' /></label><button onclick='' style='margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: white; color: green; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
     }
     console.log(masterList);
+}
+
+function deleteTask(){
+    let name = document.getElementById('SelectBar').value;
+    for (let i = 0; i < masterList[name].length; i++) {
+        let task = masterList[name][i];
+        masterList[name].splice(task)
+    }
+    loadList();
+}
+
+function clearList(){
+
+    let name = document.getElementById('SelectBar').value;
+    for (let i = 0; i < masterList[name].length; i++) {
+        let task = masterList[name][i];
+        masterList[name].splice(task)
+    }
+    loadList();
 }
