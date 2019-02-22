237154ebd8fcb487f1f4f9c3ae688422d3f2fc46
David Schlenker <ironinstrumentals@gmail.com>
2019-02-22 08:10:31 -0700

    #14 - Deleting Tasks
    finally am able to delete tasks from a list!

--- JS/Main.js
+++ JS/Main.js
@@ -3,13 +3,16 @@ masterList = {
 
 };
 
+let x = 0;
+
 let addList = function (name) {
     masterList[name] = [];
     document.getElementById('SelectBar').innerHTML += '<option id="' + name + '">' + name + '</option>'
 };
 
 
-addTaskToList = function (task) {
+addTaskToList = function () {
+    let task = document.getElementById('NewTask').value;
     let name = document.getElementById('SelectBar').value || 'unassigned';
     masterList[name].push(task);
     loadList();
@@ -18,26 +21,29 @@ function loadList(){
     let name = document.getElementById('SelectBar').value;
     document.getElementById('CurrentList').innerHTML = "";
     for (let i = 0; i < masterList[name].length; i++) {
-        document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><button onclick='deleteTask()' id='btn"+ masterList[name][i] +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input value='" + masterList[name][i] + "' /></label><button onclick='' style='margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: white; color: green; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
+        document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv'><button onclick='deleteTask(this.id)' id='"+ masterList[name][i].toString() +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input id='[Input]" + masterList[name][i] + "' value='" + masterList[name][i] + "' /></label><button onclick='' style='margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: white; color: green; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
     }
     console.log(masterList);
 }
 
-function deleteTask(){
+function deleteTask(clicked_id){
+    let task = clicked_id;
     let name = document.getElementById('SelectBar').value;
-    for (let i = 0; i < masterList[name].length; i++) {
-        let task = masterList[name][i];
-        masterList[name].splice(task)
+    if (masterList[name].indexOf(task) > 0) {
+        masterList[name].splice(masterList[name].indexOf(task), 1);
+        masterList[name].unshift(task);
     }
+    masterList[name].shift();
+    //alert(clicked_id); //- used this to ensure the value of clicked_id was the correct id.
+    //masterList[name].pull(task);
     loadList();
 }
 
 function clearList(){
-
     let name = document.getElementById('SelectBar').value;
     for (let i = 0; i < masterList[name].length; i++) {
         let task = masterList[name][i];
-        masterList[name].splice(task)
+        masterList[name].splice(task);
     }
     loadList();
 }
