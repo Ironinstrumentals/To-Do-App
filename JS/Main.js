dd17d35322eacd341f69f7b28c831f17587705db
David Schlenker <ironinstrumentals@gmail.com>
2019-02-27 08:37:23 -0700

    #22 - Publication Update 2
    Removed Comments, cleaned up script, and removed unused scripts from HTML.

--- JS/Main.js
+++ JS/Main.js
@@ -1,18 +1,11 @@
-masterList = {
-    //CacheHolder: []
-
-};
-
+masterList = {};
 let addList = function (name) {
     masterList[name] = [];
-    //document.getElementById('SelectBar').innerHTML += '<option onclick="loadList()" id="' + name + '">' + name + '</option>';
     saveCache();
     loadCache();
     document.getElementById('SelectBar').value = name;
     loadList();
 };
-
-
 addTaskToList = function () {
     let task = document.getElementById('NewTask').value;
     let name = document.getElementById('SelectBar').value || 'unassigned';
@@ -24,11 +17,12 @@ function loadList(){
     let name = document.getElementById('SelectBar').value;
     document.getElementById('CurrentList').innerHTML = "";
     for (let i = 0; i < masterList[name].length; i++) {
-        document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv' style='display: flex; width: 100%;'><button onclick='deleteTask(this.id)' id='"+ masterList[name][i].toString() +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3.5px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input class='task' id='[Input]" + masterList[name][i] + "' value='" + masterList[name][i] + "' /></label><button class='white' id='z" + masterList[name][i] + "' onclick='completeTask(this.id)' style='border: solid dodgerblue 1px; margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: dodgerblue; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
+        if (masterList[name].length > 0) {
+            document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv' style='display: flex; width: 100%;'><button onclick='deleteTask(this.id)' id='"+ masterList[name][i].toString() +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3.5px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'><i class='fas fa-times'></i></button><label><input class='task' id='[Input]" + masterList[name][i] + "' value='" + masterList[name][i] + "' /></label><button class='white' id='z" + masterList[name][i] + "' onclick='completeTask(this.id)' style='border: solid dodgerblue 1px; margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: dodgerblue; border-radius: 5px;'><i class='fas fa-check'></i></button></div>"
+        }
     }
     saveCache();
 }
-
 function deleteTask(clicked_id){
     let task = clicked_id;
     let name = document.getElementById('SelectBar').value;
@@ -40,7 +34,6 @@ function deleteTask(clicked_id){
     saveCache();
     loadList();
 }
-
 function deleteList(){
     clearList();
     let name = document.getElementById('SelectBar').value;
@@ -48,7 +41,6 @@ function deleteList(){
     saveCache();
     loadCache();
 }
-
 function clearList(){
     let name = document.getElementById('SelectBar').value;
     for (let i = 0; i < masterList[name].length; i++) {
@@ -57,27 +49,17 @@ function clearList(){
     }
     loadList();
 }
-
 function completeTask(selectedButton){
     document.getElementById(selectedButton).classList.toggle('white');
     document.getElementById(selectedButton).classList.toggle('completed');
 }
-
-
-
 function saveCache(){
-    //console.log(masterList);
     let masterList_serialized = JSON.stringify(masterList);
     localStorage.setItem('Cache', masterList_serialized);
-    //console.log(masterList_serialized);
 }
-
 function clearCache(){ // Used for Error Debugging.
     localStorage.clear();
 }
-
-
-
 function loadCache() {
     if (localStorage.getItem('Cache')) {
         document.getElementById('SelectBar').innerHTML = "";
@@ -87,6 +69,3 @@ function loadCache() {
         }
     }
 }
-
-
-/////////
