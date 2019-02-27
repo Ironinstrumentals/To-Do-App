92a84f2dd31873766671896110ddfb9e7708aa6f
David Schlenker <ironinstrumentals@gmail.com>
2019-02-27 13:42:03 -0700

    #26 - Error Catching
    Added Error Catching for when the list is undefined/ no list exists.

--- JS/Main.js
+++ JS/Main.js
@@ -20,17 +20,19 @@ addTaskToList = function () {
 function loadList(){
     let name = document.getElementById('SelectBar').value;
     document.getElementById('CurrentList').innerHTML = "";
-    for (let i = 0; i < masterList[name].length; i++) {
-        if (masterList[name].length > 0) {
-            document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv fadeIn' style='display: flex; width: 100%;'>" +
-                "<button onclick='deleteTask(this.id)' id='"+ masterList[name][i].toString() +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3.5px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'>" +
-                "<i class='fas fa-times'></i></button>" +
-                "<label><input onchange='updateList(this.id);' class='task' id='x" + masterList[name][i] + "' value='" + masterList[name][i] + "' /></label>" +
-                "<button class='white' id='z" + masterList[name][i] + "' onclick='completeTask(this.id)' style='border: solid dodgerblue 1px; margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: dodgerblue; border-radius: 5px;'>" +
-                "<i class='fas fa-check'></i></button></div>"
+    if (typeof masterList[name] !== 'undefined'){
+        for (let i = 0; i < masterList[name].length; i++) {
+            if (masterList[name].length > 0) {
+                document.getElementById('CurrentList').innerHTML += "<div class='FakeDiv fadeIn' style='display: flex; width: 100%;'>" +
+                    "<button onclick='deleteTask(this.id)' id='"+ masterList[name][i].toString() +"' style='margin-right: 3px; padding-top: 1.5px; padding-left: 3.5px; padding-right: 3.5px; background-color: red; color: white; border-radius: 5px;'>" +
+                    "<i class='fas fa-times'></i></button>" +
+                    "<label><input onchange='updateList(this.id);' class='task' id='x" + masterList[name][i] + "' value='" + masterList[name][i] + "' /></label>" +
+                    "<button class='white' id='z" + masterList[name][i] + "' onclick='completeTask(this.id)' style='border: solid dodgerblue 1px; margin-left: 3px; padding-top: 1.5px; padding-left: 3px; padding-right: 3.5px; background-color: dodgerblue; border-radius: 5px;'>" +
+                    "<i class='fas fa-check'></i></button></div>"
+            }
         }
+        saveCache();
     }
-    saveCache();
 }
 function deleteTask(clicked_id){
     let task = clicked_id;
